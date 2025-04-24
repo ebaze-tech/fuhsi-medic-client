564d7c206e2f680da4453b225bf765407231940a
ebaze-tech <ebaze.technologies@gmail.com>
2025-04-24 16:47:53 +0100

    Started client side

---
+++ api.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+// Use the environment variable for the base URL
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Add JWT token to every request (if available)
+API.interceptors.request.use(
+  (req) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      req.headers.Authorization = `Bearer ${token}`;
+    }
+    return req;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default API;
